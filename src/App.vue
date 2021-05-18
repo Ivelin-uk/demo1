56ac264a52ecc8d37356378fe64610b23af51594
Ivelin Mihaylov <ivoagaininwow@gmail.com>
2021-05-18 15:59:33 +0300

    drawer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,47 @@
       v-model="drawer"
       app
     >
-      <router-link to="/">Home</router-link>
-      <router-link to="/patients">Patients</router-link>
-      <router-link to="/storage">Storage</router-link>
+      <template>
+        <v-card
+          height="400"
+          width="256"
+          class="mx-auto"
+        >
+          <v-navigation-drawer permanent>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="title">
+                  Application
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  subtext
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list
+              dense
+              nav
+            >
+              <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+              >
+                <v-list-item-icon>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                  <v-list-item-title><router-link to="/">{{ item.title }}</router-link></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+        </v-card>
+      </template>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -26,9 +64,9 @@
     data () {
       return {
         items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'Patients', icon: 'mdi-image' },
+          { title: 'Storage', icon: 'mdi-help-box' },
         ],
         right: null,
         drawer: null
