6ba5714335ac6280a1daaa65751eb8f0c4364d26
Ivelin Mihaylov <ivoagaininwow@gmail.com>
2021-05-21 16:37:32 +0300

    add and remove user

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-        <h1>Users</h1>
+        <h1>Users {{massega}}</h1>
         <app-input></app-input>
+        <div>
+            <v-btn color="primary" elevation="2" v-on:click="Add">Add Name</v-btn>
+            <ul id="example-1">
+                <li v-for="(item, index) in names" :key="item.firstName" :id="index">
+                    <p><span>{{item.firstName}}</span> <span v-on:click="Delete">Delete</span></p>
+                </li>
+            </ul>
+            <v-btn color="primary" elevation="2" v-on:click="event">Add 1.</v-btn>
+            <p>The button above has been clicked {{ counter }} times.</p>
+        </div>
     </div>
 </template>
 
@@ -9,8 +19,30 @@
 import input from '@/components/input.vue'
 
 export default ({
+    data(){
+        return{
+            massega: 'work',
+            names: [
+                { firstName: 'pesho' },
+                { firstName: 'Pesho' },
+                { firstName: 'Ivan' },
+                { firstName: 'Gosho' },
+            ],
+            counter: 0,
+        }
+    },
     components: {
         'app-input': input,
+    },
+    methods: {
+        Add: function() {
+            this.names.push({ firstName: 'pesho2' });
+        },
+        Delete: function(event) {
+            let id = event.target.parentElement.parentElement.id;
+            console.log(id);
+            this.names.splice(id, 1);
+        },
     }
 })
 </script>
